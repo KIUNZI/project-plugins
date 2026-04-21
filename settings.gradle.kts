2c76f01e4b63075720cae437cde0017b0ce6600b
jamarsto <jason@jasonmarston.me.uk>
2026-04-21 18:47:06 +0100

    Cleanup of naming

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
-  includeBuild("../settings-plugin")
+  includeBuild("../kiunzi-settings-plugin")
 }
 
-includeBuild("../settings-plugin")
+includeBuild("../kiunzi-settings-plugin")
 
 plugins {
   id("uk.co.jasonmarston.standards.settings")
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name="project-plugins"
+rootProject.name="kiunzi-project-plugins"
